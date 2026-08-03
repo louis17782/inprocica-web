f047d4e0f84a4c91596830c2c952e80c73d7c501
louis17782 <luissanteliz22@gmail.com>
2026-08-03 15:06:28 -0400

    replace url production

--- others/ValidationForm.js
+++ others/ValidationForm.js
@@ -24,7 +24,7 @@ const form = document.getElementById('contact-form');
     }
 
     try {
-      const response = await fetch('http://localhost:3000/send-email', {
+      const response = await fetch('https://resendmailer-inprocica-landingpage-expressmailer.up.railway.app/send-email', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
